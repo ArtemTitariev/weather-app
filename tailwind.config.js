ac8b30813c4e79854c2e98c02067480865abb137
Artem Titariev <artem.titariev@gmail.com>
2024-05-21 09:50:20 +0300

    [main] Edit theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,29 +7,23 @@ module.exports = {
     './resources/**/*.vue',
   ],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      primary: '#2563EB',
-      accent: '#1E40AF',
-      secondary: '#D97706',
-      success: '#10B981',
-      danger: '#EF4444',
-      warning: '#F59E0B',
-      info: '#3B82F6',
-      light: '#F3F4F6',
-      dark: '#111827',
-    },
-    fontFamily: {
-      sans: ['ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
-      serif: ['ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
-      mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
-    },
     extend: {
+      colors: {
+        primary: '#2563EB',
+        accent: '#1E40AF',
+        secondary: '#D97706',
+        success: '#10B981',
+        danger: '#EF4444',
+        warning: '#F59E0B',
+        info: '#3B82F6',
+        light: '#F3F4F6',
+        dark: '#111827',
+      },
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+        serif: ['ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+      },
       spacing: {
         '128': '32rem',
         '144': '36rem',
@@ -39,21 +33,4 @@ module.exports = {
       }
     }
   }
-}
-
-
-export default {
-  content: [
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.vue',
-    './resources/css/**/*.css',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        
-      },
-    },
-  },
-  plugins: [],
 }
