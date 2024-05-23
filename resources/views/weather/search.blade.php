18108ac1e6c661bab485bf888f815043f7eb5c63
Valeriia Sydorenko <valeriia.sydorenko@student.sumdu.edu.ua>
2024-05-23 13:31:56 +0300

    Change cities page styles, add js

--- resources/views/weather/search.blade.php
+++ resources/views/weather/search.blade.php
@@ -12,42 +12,44 @@
     <h1 class="text-3xl font-bold mb-4 text-center">Search for Weather Data</h1>
 
     <div class="grid grid-cols-3 gap-4 mb-4">
-        <div class="bg-blue-100 p-4 rounded text-center">
+        <div id="step-1" class="bg-primary text-white p-4 rounded text-center">
             <h2 class="font-bold text-xl">Крок 1</h2>
             <p>Оберіть дати</p>
         </div>
-        <div class="bg-blue-100 p-4 rounded text-center">
+        <div id="step-2" class="bg-primary text-white p-4 rounded text-center">
             <h2 class="font-bold text-xl">Крок 2</h2>
             <p>Оберіть місто</p>
         </div>
-        <div class="bg-blue-100 p-4 rounded text-center">
+        <div id="step-3" class="bg-primary text-white p-4 rounded text-center">
             <h2 class="font-bold text-xl">Крок 3</h2>
             <p>Виконайте пошук</p>
         </div>
     </div>
 
-    <form method="GET" action="{{ route('weather.index') }}">
+    <form method="GET" action="{{ route('weather.index') }}" id="search-form" class="mb-4">
         @csrf
         <x-forms.input-container>
-            <label for="start_date" class="block text-gray-700">Start Date</label>
-            <input type="date" id="start_date" name="start_date" class="w-full p-2 border rounded mt-1" required>
+            <x-forms.label for="start_date" class="block text-gray-700">Start Date</x-forms.label>
+            <x-forms.input type="date" id="start_date" name="start_date" required />
         </x-forms.input-container>
 
         <x-forms.input-container>
-            <label for="end_date" class="block text-gray-700">End Date</label>
-            <input type="date" id="end_date" name="end_date" class="w-full p-2 border rounded mt-1" required>
+            <x-forms.label for="end_date" class="block text-gray">End Date</x-forms.label>
+            <x-forms.input type="date" id="end_date" name="end_date" required />
         </x-forms.input-container>
 
         <x-forms.input-container>
-            <label for="city_search" class="block text-gray-700">Search City</label>
-            <input type="text" id="city_search" class="w-full p-2 border rounded mt-1" onkeyup="filterCities()" placeholder="Type to search...">
+            <x-forms.label for="city_search" class="block text-gray ">Search City</x-forms.label>
+            <x-forms.input type="text" id="city_search" name="city_search" placeholder="Type to search..." />
         </x-forms.input-container>
-
-        <x-forms.button type="submit" class="mb-4">Search</x-forms.button>
+        
+        <x-forms.input-container>
+            <x-forms.button type="submit" id="submit-button" disabled class="bg-primary text-white hover:bg-accent">Search</x-forms.button>
+        </x-forms.input-container>
 
         <div id="cities_list" class="grid grid-cols-1 md:grid-cols-2 gap-4">
             @foreach ($cities as $city)
-                <label class="city-card p-4 border rounded bg-white shadow block cursor-pointer">
+                <label class="city-card p-4 border border-primary rounded bg-white shadow block cursor-pointer">
                     <h2 class="text-xl font-bold">{{ $city->name }}</h2>
                     <p>Latitude: {{ $city->lat }}</p>
                     <p>Longitude: {{ $city->lon }}</p>
@@ -62,23 +64,94 @@
 
 @section('scripts')
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('.city-card').click(function() {
-            $('.city-card').removeClass('bg-blue-200 border-blue-500');
-            $(this).addClass('bg-blue-200 border-blue-500');
-        });
 
-        $('#city_search').keyup(function() {
-            let filter = $(this).val().toLowerCase();
-            $('.city-card').each(function() {
-                let city = $(this).text().toLowerCase();
-                if (city.includes(filter)) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
+function checkRequiredFields(formId) {
+    let isFormValid = true;
+
+    $(`#${formId} [required]`).each(function() {
+        const fieldType = $(this).attr('type');
+
+        if (fieldType === 'radio' || fieldType === 'checkbox') {
+            if (!$(`input[name="${$(this).attr('name')}"]:checked`).length) {
+                isFormValid = false;
+            }
+        } else if ($(this).is('select')) {
+            if ($(this).val() === null || $(this).val() === '') {
+                isFormValid = false;
+            }
+        } else {
+            if ($(this).is(':invalid') || $(this).val() === '') {
+                isFormValid = false;
+            }
+        }
+    });
+
+    return isFormValid;
+}
+
+function checkFormValidity(formId) {
+    let isFormValid = checkRequiredFields(formId);
+    $(`#${formId} #submit-button`).prop('disabled', !isFormValid);
+
+    if (isFormValid) {
+        $(`#${formId} #submit-button`).removeClass('bg-gray cursor-not-allowed').addClass('bg-primary hover:bg-accent cursor-pointer');
+    } else {
+        $(`#${formId} #submit-button`).removeClass('bg-primary hover:bg-accent cursor-pointer').addClass('bg-gray cursor-not-allowed');
+    }
+
+    updateStepStyles(formId);
+}
+
+function updateStepStyles(formId) {
+    if ($(`#${formId} #start_date`).val() && $(`#${formId} #end_date`).val()) {
+        $('#step-1').removeClass('bg-primary').addClass('bg-secondary');
+    } else {
+        $('#step-1').removeClass('bg-secondary').addClass('bg-primary');
+    }
+
+    if ($(`#${formId} input[name="city_id"]:checked`).length > 0) {
+        $('#step-2').removeClass('bg-primary').addClass('bg-secondary');
+    } else {
+        $('#step-2').removeClass('bg-secondary').addClass('bg-primary');
+    }
+}
+
+function combined(formId) {
+    checkFormValidity(formId);
+    updateStepStyles(formId);
+}
+
+$(document).ready(function() {
+    const formId = 'search-form';
+
+    $('.city-card').click(function() {
+        $('.city-card').removeClass('border-secondary').addClass('border-primary');
+        $('.city-card h2').removeClass('text-secondary');
+
+        $(this).removeClass('border-primary').addClass('border-secondary');
+        $(this).find('h2').addClass('text-secondary');
+        $(this).find('input[type="radio"]').prop('checked', true);
+        // checkFormValidity(formId);
+        combined(formId);
+    });
+
+    $('#city_search').keyup(function() {
+        let filter = $(this).val().toLowerCase();
+        $('.city-card').each(function() {
+            let city = $(this).text().toLowerCase();
+            if (city.includes(filter)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
         });
     });
+
+    checkFormValidity(formId);
+    $(`#${formId} [required]`).on('change keyup', function() {
+        combined(formId);
+    });
+
+});
 </script>
 @endsection
