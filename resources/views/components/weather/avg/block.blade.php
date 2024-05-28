62ab58166efb4c5e17daeb16784485c0ec4bcebf
Artem Titariev <artem.titariev@gmail.com>
2024-05-28 13:42:02 +0300

    [main] Use short attribute syntax

--- resources/views/components/weather/avg/block.blade.php
+++ resources/views/components/weather/avg/block.blade.php
@@ -1,7 +1,7 @@
 @props(['src', 'alt', 'title', 'text'])
 
 <div {{ $attributes->merge(['class' => 'bg-light p-4 rounded shadow flex items-center']) }}>
-    <x-weather.avg.img :src="$src" :alt="$alt" />
+    <x-weather.avg.img :$src :$alt />
     <div>
         <x-weather.avg.h>{{ $title }}</x-weather.avg.h>
         <x-weather.avg.p>{{ $text }}</x-weather.avg.p>
--- resources/views/components/weather/data/block.blade.php
+++ resources/views/components/weather/data/block.blade.php
@@ -1,6 +1,6 @@
 @props(['src', 'alt', 'text'])
 
 <div {{ $attributes->merge(['class' => 'flex items-center mb-2']) }}>
-    <x-weather.data.img :src="$src" :alt="$alt" />
+    <x-weather.data.img :$src :$alt />
     <x-weather.data.p>{{ $text }}</x-weather.data.p>
 </div>
